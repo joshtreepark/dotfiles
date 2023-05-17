46ea089a1ffea2ed27afb8a89b59d8685eccde7e
Joshua Park <joshua.park@yale.edu>
2023-05-17 16:56:02 -0400

    it's pretty hard to convert all plugins to lazy.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,42 +1,28 @@
-require "user.options"
-require "user.impatient"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.cmp"
-require "user.lualine"
-require "user.vim-slime"
-require "user.telescope"
-require "user.treesitter"
-require "user.gitsigns"
-require "user.indent-blankline"
-require "user.mason"
-require "user.lsp" -- should come after mason
-require "user.others"
-require "user.alpha"
--- require("functions")
-require "user.luasnip"
+vim.g.mapleader = " "
 
-vim.cmd([[
-augroup numbertoggle
-  autocmd!
-  autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
-  autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
-augroup END
-]])
+require('config.lazy')
+require('config.options')
 
--- For WSL Clipboard functionality
-vim.cmd([[
-let g:clipboard = {
-    \   'name': 'WslClipboard',
-    \   'copy': {
-    \      '+': 'clip.exe',
-    \      '*': 'clip.exe',
-    \    },
-    \   'paste': {
-    \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    \   },
-    \   'cache_enabled': 0,
-    \ }
-]])
+-- vim.cmd([[
+-- augroup numbertoggle
+--   autocmd!
+--   autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
+--   autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
+-- augroup END
+-- ]])
+--
+-- -- For WSL Clipboard functionality
+-- vim.cmd([[
+-- let g:clipboard = {
+--     \   'name': 'WslClipboard',
+--     \   'copy': {
+--     \      '+': 'clip.exe',
+--     \      '*': 'clip.exe',
+--     \    },
+--     \   'paste': {
+--     \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--     \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--     \   },
+--     \   'cache_enabled': 0,
+--     \ }
+-- ]])
