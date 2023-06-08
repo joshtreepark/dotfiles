5831d3e1ca0c5a0856f0bf25129998aa9f92b665
Joshua Park <joshua.park@yale.edu>
2023-06-08 15:29:53 -0400

    change to tabs=4 and add zen-mode

--- nvim/lua/old/indent-blankline.lua
+++ nvim/lua/old/indent-blankline.lua
@@ -3,20 +3,20 @@ vim.opt.list = true
 -- vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
-  filetype_exclude = {
-    "help",
-    "terminal",
-    "alpha",
-    "packer",
-    "lspinfo",
-    "TelescopePrompt",
-    "TelescopeResults",
-    "mason",
-    "",
-  },
-  buftype_exclude = { "terminal" },
-  show_first_indent_level = false,
-  -- space_char_blankline = " ",
-  -- show_current_context = true,
-  -- show_current_context_start = true,
+    filetype_exclude = {
+        "help",
+        "terminal",
+        "alpha",
+        "packer",
+        "lspinfo",
+        "TelescopePrompt",
+        "TelescopeResults",
+        "mason",
+        "",
+    },
+    buftype_exclude = { "terminal" },
+    show_first_indent_level = false,
+    -- space_char_blankline = " ",
+    -- show_current_context = true,
+    -- show_current_context_start = true,
 }
